86745c0614a23bb9352cc9b4fdf7d787e60786a9
Heirer <60943650+Heirer@users.noreply.github.com>
2022-11-23 05:04:19 -0600

    8

--- ProyectoPI/ProyectoPI/DeteccionMovimiento.Designer.cs
+++ ProyectoPI/ProyectoPI/DeteccionMovimiento.Designer.cs
@@ -1,8 +1,7 @@
-﻿using AForge.Video;
-using AForge.Video.DirectShow;
-using AForge.Vision.Motion;
+﻿
 namespace ProyectoPI
 {
+
     partial class DeteccionMovimiento
     {
         /// <summary>
@@ -31,19 +30,75 @@ namespace ProyectoPI
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(49, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(275, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(49, 92);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(579, 304);
+            this.videoSourcePlayer1.TabIndex = 1;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(382, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(490, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Detener";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // DeteccionMovimiento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.ClientSize = new System.Drawing.Size(784, 502);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Controls.Add(this.comboBox1);
             this.Name = "DeteccionMovimiento";
             this.Text = "DeteccionMovimiento";
+            this.Load += new System.EventHandler(this.DeteccionMovimiento_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- ProyectoPI/ProyectoPI/DeteccionMovimiento.cs
+++ ProyectoPI/ProyectoPI/DeteccionMovimiento.cs
@@ -8,13 +8,51 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision.Motion;
+
 namespace ProyectoPI
 {
     public partial class DeteccionMovimiento : Form
     {
+
+        //AGREGAR REFERENCIAS
+        //AGREGAR CNTROLES
+        //DISEÑAR FORMULARIO
+        //VideoSourcePlayer1 es el control donde se motrara la imagen de la webcam
+
         public DeteccionMovimiento()
         {
             InitializeComponent();
+        }
+        //Variable para lista de dispositivos
+        private FilterInfoCollection Dispositivos;
+
+        //variable para fuente de  video
+        private VideoCaptureDevice FuenteDeVideo;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeteccionMovimiento_Load(object sender, EventArgs e)
+        {
+
+            Dispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            // cargar todos los disositivos al combo
+            foreach  (FilterInfo x in Dispositivos)
+                {
+                comboBox1.Items.Add(x.Name);            
+                }
+            comboBox1.SelectedIndex = 0;
         }
     }
 }
